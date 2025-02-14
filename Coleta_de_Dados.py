ebf64a076267055d2b8a6fed1f0cbd04b86eda9f
RhuanMateusCMB <rhuanmateuscmb@gmail.com>
2025-02-14 14:11:54 -0300

    Update Coleta_de_Dados.py

--- Coleta_de_Dados.py
+++ Coleta_de_Dados.py
@@ -157,7 +157,7 @@ def configurar_driver():
     options.add_experimental_option('useAutomationExtension', False)
     options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
     
-    
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1920,1080')
